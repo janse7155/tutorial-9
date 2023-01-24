fee215df0832872871687e5f68a03541de72e8f2
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2023-01-24 09:12:27 -0600

    add countdown

--- tny_script.js
+++ tny_script.js
@@ -14,6 +14,10 @@
 /* Display the current date and time */
 document.getElementById("dateNow").innerHTML = "m/d/y<br />h:m:s";
 
-
+/* Display the time left until New Year's Eve*/
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
 
 window.alert("Welcome to Tulsa");
